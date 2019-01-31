bde9895dfc952e5ec7e4754f70226fc70d3dc096
WangWang3219 <2459145973@qq.com>
2019-02-01 01:28:12 +0800

    NULL

--- YouDao.py
+++ YouDao.py
@@ -5,27 +5,27 @@ import urllib.request
 def trans():
     content = input("请输入需要翻译的内容：")
     url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
-    # 创建data字典，存储上图的Form Data
-    data = {}
-    data['i'] = content
-    data['from'] = 'AUTO'
-    data['to'] = 'AUTO'
-    data['smartresult'] = 'dict'
-    data['client'] = 'fanyideskweb'
-    # data['sign'] = '1b4945929576aa885ae480f99cc87a3f'
-    # data['salt'] = '1508333076510'
-    data['doctype'] = 'json'
-    data['version'] = '2.1'
-    data['keyfrom'] = 'fanyi.web'
-    data['action'] = 'FY_BY_CLICKBUTTION'
-    data['typoResult'] = 'true'
+    # 创建Form_Data字典，存储上图的Form Data
+    Form_Data = {}
+    Form_Data['i'] = content
+    Form_Data['from'] = 'AUTO'
+    Form_Data['to'] = 'AUTO'
+    Form_Data['smartresult'] = 'dict'
+    Form_Data['client'] = 'fanyideskweb'
+    # Form_Data['sign'] = '1b4945929576aa885ae480f99cc87a3f'
+    # Form_Data['salt'] = '1508333076510'
+    Form_Data['doctype'] = 'json'
+    Form_Data['version'] = '2.1'
+    Form_Data['keyfrom'] = 'fanyi.web'
+    Form_Data['action'] = 'FY_BY_CLICKBUTTION'
+    Form_Data['typoResult'] = 'true'
     # 使用urlencode方法转换标准格式
-    data = urllib.parse.urlencode(data).encode('utf-8')
+    data = urllib.parse.urlencode(Form_Data).encode('utf-8')
     # 传递Request对象和转换完格式的数据
     response = urllib.request.urlopen(url, data)
     # 读取信息并解码
     html = response.read().decode('utf-8')
-    # 使用JSON
+    # 将已编码的 JSON 字符串解码为 Python 对象
     html = json.loads(html)
     # 找到翻译结果
     target = html['translateResult'][0][0]['tgt']
